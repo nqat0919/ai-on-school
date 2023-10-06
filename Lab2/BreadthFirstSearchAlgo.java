485267d364e6018cace73e1146c98e28fcd720d1
Nguyen Tuan <nqat0919@gmail.com>
2023-10-06 16:10:30 +0700

    Uploading lab 2

---
+++ Lab2/BreadthFirstSearchAlgo.java
@@ -0,0 +1,68 @@
+package Lab2;
+
+import java.util.*;
+
+public class BreadthFirstSearchAlgo implements ISearchAlgo{
+
+    @Override
+    public Node execute(Node root, String goal) {
+        Queue<Node> frontier = new LinkedList<Node>();
+        List<Node> explored = new ArrayList<>();
+        frontier.offer(root);
+        while (!frontier.isEmpty()) {
+            Node current = frontier.poll();
+            if (current.getLabel().equals(goal)) return current;
+            for (Node child : current.getChildrenNodes()) {
+                if (frontier.contains(child) || explored.contains(child)) continue;
+                frontier.offer(child);
+                child.setParent(current);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Node execute(Node root, String start, String goal) {
+        boolean check = false;
+        Queue<Node> frontier = new LinkedList<Node>();
+        List<Node> explored = new ArrayList<>();
+        frontier.offer(root);
+        while (!frontier.isEmpty()) {
+            Node current = frontier.poll();
+            // meet the start point
+            if (current.getLabel().equals(start) && !check) {
+                check = true;
+                frontier.clear();
+                explored.clear();
+                current.setParent(null);
+                frontier.add(current);
+                continue;
+            }
+            if (current.getLabel().equals(goal) && check) return current;
+            for (Node child : current.getChildrenNodes()) {
+                if (frontier.contains(child) || explored.contains(child)) continue;
+                frontier.offer(child);
+                child.setParent(current);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Node nodeS = new Node("S");
+        Node nodeA = new Node("A"); Node nodeB = new Node("B");
+        Node nodeC = new Node("C"); Node nodeD = new Node("D");
+        Node nodeE = new Node("E"); Node nodeF = new Node("F");
+        Node nodeG = new Node("G"); Node nodeH = new Node("H");
+        nodeS.addEdge(nodeA, 5); nodeS.addEdge(nodeB, 2);
+        nodeS.addEdge(nodeC, 4); nodeA.addEdge(nodeD, 9);
+        nodeA.addEdge(nodeE, 4); nodeB.addEdge(nodeG, 6);
+        nodeC.addEdge(nodeF, 2); nodeD.addEdge(nodeH, 7);
+        nodeE.addEdge(nodeG, 6); nodeF.addEdge(nodeG, 1);
+        ISearchAlgo algo1 = new BreadthFirstSearchAlgo();
+        Node result = algo1.execute(nodeS, "G");
+        System.out.println(NodeUtils.printPath(result));
+        result = algo1.execute(nodeS, "A", "G");
+        System.out.println(NodeUtils.printPath(result));
+    }
+}
